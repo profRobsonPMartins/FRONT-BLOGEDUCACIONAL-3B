2fb9959bece6f437e82358228782b56e51aba2f5
Robson Pereira Martins <profrobson.p.martins2023@gmail.com>
2024-12-03 12:48:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,52 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // Conteúdo
+import Header from './components/Header/Header';
+import AssuntosDestaque from './components/Assuntos/AssuntosDestaque';
 import CardsEducacionais from './components/Cards/CardsEducacionais';
+import LatestBlogs from './components/Latest/LatestBlogs';
 import Contact from './components/Contact/Contact';
+import Testimonials from './components/Testimonials/Testimonials';
+import Footer from './components/Footer/Footer';
 
 // Pages
 import Hero from './pages/Hero';
+import MathContent from './pages/MathContent';
+import PhysicsContent from './pages/PhysicsContent';
+import ChemistryContent from './pages/ChemistryContent';
+import HistoryContent from './pages/HistoryContent';
+import GeographyContent from './pages/GeographyContent';
+import BiologyContent from './pages/BiologyContent';
+import LiteratureContent from './pages/LiteratureContent';
+import SociologyContent from './pages/SociologyContent';
+import PhilosophyContent from './pages/PhilosophyContent';
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path='/' element={
           <>
-             <Hero />
-             <CardsEducacionais />
-             <Contact />
+            <Hero />
+            <AssuntosDestaque />
+            <CardsEducacionais />
+            <LatestBlogs />
+            <Contact />
+            <Testimonials />
           </>
         } />
-        
+        <Route path='/matematica' element={<MathContent />} />
+        <Route path='/fisica' element={<PhysicsContent />} />
+        <Route path='/quimica' element={<ChemistryContent />} />
+        <Route path='/historia' element={<HistoryContent />} />
+        <Route path='/geografia' element={<GeographyContent />} />
+        <Route path='/biologia' element={<BiologyContent />} />
+        <Route path='/literatura' element={<LiteratureContent />} />
+        <Route path='/sociologia' element={<SociologyContent />} />
+        <Route path='/filosofia' element={<PhilosophyContent />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Assuntos/AssuntosDestaque.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import '../../styles/AssuntosDestaque.css';
+
+const AssuntosDestaque = () => {
+    return (
+        <section className="featured-topics">
+            <h2>Assuntos em Destaque</h2>
+            <p>Explore temas que desenvolvem habilidades práticas e profissionais, como artes culinárias, programação de computadores, design gráfico e muito mais.</p>
+            <div className="topics-container">
+                <div className="topic-card">
+                    <i className="fas fa-code"></i>
+                    <h3>Programação</h3>
+                    <p>Aprenda a criar aplicativos e resolver problemas através da lógica e da criatividade.</p>
+                    <button className="topic-button" onClick={() => window.open('https://www.w3schools.com', '_blank')}>
+                        Saiba Mais
+                    </button>
+                </div>
+
+                <div className="topic-card">
+                    <i className="fas fa-leaf"></i>
+                    <h3>Meio Ambiente</h3>
+                    <p>Explore questões ambientais e descubra como proteger nosso planeta.</p>
+                    <button className="topic-button" onClick={() => window.open('https://ipecpesquisas.org.br/projetobotocinza/', '_blank')}>
+                        Saiba Mais
+                    </button>
+                </div>
+
+                <div className="topic-card">
+                    <i className="fas fa-book"></i>
+                    <h3>Leitura</h3>
+                    <p>Descubra o poder da leitura e como ela pode enriquecer seu conhecimento e criatividade.</p>
+                    <button className="topic-button" onClick={() => window.open('https://www.todamateria.com.br/lingua-portuguesa/', '_blank')}>
+                        Saiba Mais
+                    </button>
+                </div>
+
+                <div className="topic-card">
+                    <i className="fas fa-balance-scale"></i>
+                    <h3>Equilíbrio</h3>
+                    <p>Aprenda sobre a importância do equilíbrio entre vida pessoal e profissional.</p>
+                    <button className="topic-button" onClick={() => window.open('https://tcc-psicologia.onrender.com/', '_blank')}>
+                        Saiba Mais
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default AssuntosDestaque;
